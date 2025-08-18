822bc4408c848ca79e01164e15cdb1bb1f18f1c4
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-08-18 16:36:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    private:
+     void dfs(int node, vector<int>& vis, stack<int>& st, vector<vector<int>>& adj){
+         vis[node]=1;
+         
+         for(auto it : adj[node]){
+             if(!vis[it]){
+                 dfs(it,vis,st,adj);
+             }
+         }
+         st.push(node);
+     }
+  public:
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        vector<vector<int>>adj(V);
+        vector<int>vis(V,0);
+        stack<int>st;
+        
+        for(auto e : edges){
+            int u=e[0];
+            int v=e[1];
+            
+            adj[u].emplace_back(v);
+            
+        }
+        
+        for(int i=0; i<V; i++){
+            if(!vis[i]){
+                dfs(i,vis,st,adj);
+            }
+        }
+        
+        vector<int>ans;
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        return ans;
+    }
+};
