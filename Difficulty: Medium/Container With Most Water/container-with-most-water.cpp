d9d856f357f684288ce2a2cd2f20a61dc0667987
rohansahu022003 <161516796+rohansahu022003@users.noreply.github.com>
2025-10-04 23:19:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Container With Most Water/container-with-most-water.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int maxWater(vector<int> &height) {
+      int i=0;
+        int j=height.size()-1;
+        int maxarea=0;
+        while(i<j){
+            int b=min(height[i],height[j]);
+            int l=j-i;
+            ;maxarea=max(maxarea,b*l);
+            if(height[i]<height[j])i++;
+            else j--;
+
+          
+
+        }
+        return maxarea;
+    }
+};
